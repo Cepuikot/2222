70a1443a62318504c3dfe02e485c9221ca65b5da
maxim.tselm@gmail.com <heshtekianegey@gmail.com>
2021-01-27 22:00:47 +0200

    ggg

---
+++ cal.py
@@ -0,0 +1,33 @@
+#калькулятор
+
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
+print( Back.CYAN )
+
+what = input( "Что выбираешь? (+, -, *, /): " )
+
+print( Back.MAGENTA )
+
+a = float( input("Первое число: ") )
+b = float( input("Второе число: ") )
+
+print( Back.RED )
+
+if what == "+":
+    c = a + b
+    print("равно:  " + str(c))
+elif what == "-":
+    c = a - b
+    print("равно:  " + str(c))
+elif what == "*":
+    c = a * b
+    print("равно:  " + str(c))
+elif what == "/":
+    c = a / b
+    print("равно:" + str(c))
+else:
+    print("такого нету(")
+
+input()
